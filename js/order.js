0e39a4ca3d0c273f58228cc893eb1c8f903b5d92
Aaron Eugene Kester <aaron.opta@yahoo.com>
2017-04-13 13:08:40 -0700

    added dynamic add item event handler, COMPLETE and ready to deploy

--- js/order.js
+++ js/order.js
@@ -1,6 +1,7 @@
 window.onload = function main() {
-createMenu()
-  // createMenuBtnHandler()
+  $('.alert').hide()
+  createMenu()
+  createMenuBtnHandler()
 }
 
 let items = []
@@ -22,7 +23,7 @@ let menuObjArr = [{
   name: 'Ice Cream Biscut',
   price: 7.99
 }, {
-  id: 'other pizza',
+  id: 'hero',
   name: `Wes's Pizza`,
   price: 3.44
 }]
@@ -30,19 +31,35 @@ let menuObjArr = [{
 function createMenu() {
   menuObjArr.forEach(function(item) {
     $('.menu-card-row').append(`<div class="thumbnail col-xs-6">
-      <img class="img" src="img/${item.id}.jpg">
-      <div class="caption">
-        <h4>${item.name}</h4>
-        <p>$${item.price}</p>
-        <hr>
-        <p><a id="${item.id}-button" href="#" class="btn btn-sm add-order-btn">ADD TO ORDER</a></p>
-      </div>
+    <img class="img" src="img/${item.id}.jpg">
+    <div class="caption">
+    <h4>${item.name}</h4>
+    <p>$${item.price}</p>
+    <hr>
+    <p><a id="${item.id}-button" href="#" class="btn btn-sm add-order-btn">ADD TO ORDER</a></p>
+    </div>
     </div>`)
   })
 }
 
+
 function createMenuBtnHandler() {
-  
+  menuObjArr.forEach(function(item, index) {
+    $(`#${item.id}-button`).on('click', function () {
+      event.preventDefault()
+      pushItems(index)
+      let itemRow = `<div class="row list-item-unindented"><small id="${item.id}-label">${item.name}</small>
+      </div>`
+      let priceRow = `<div class="row list-item-unindented"><small>$${item.price}</small></div>`
+      $('#item-listed').prepend(itemRow)
+      $('#price-listed').prepend(priceRow)
+
+      //replace totals
+      $('#subtotal-amnt').html('$' + calcSubTotal(items))
+      $('#tax-amnt').html('$' + calcTax())
+      $('#total-amnt').html('$' + calcTotal())
+    })
+  })
 }
 
 function pushItems(x) {
@@ -73,70 +90,6 @@ function calcTotal() {
   let total = Math.round(totalLong * 100)/100
   return total
 }
-
-$('.alert').hide()
-// appender code
-let itemNameDiv = "#item-listed"
-let itemPriceDiv = "#price-listed"
-
-
-$('#burger-button').on('click', function () {
-  event.preventDefault()
-  pushItems(0)
-  let burgerRow = `<div class="row list-item-unindented"><small id="burger-label">Royale with Cheese</small>
-  </div>`
-  let burgerPriceRow = `<div class="row list-item-unindented"><small>$8.99</small></div>`
-  $(itemNameDiv).prepend(burgerRow)
-  $(itemPriceDiv).prepend(burgerPriceRow)
-
-  //replace totals
-  $('#subtotal-amnt').html('$' + calcSubTotal(items))
-  $('#tax-amnt').html('$' + calcTax())
-  $('#total-amnt').html('$' + calcTotal())
-})
-$('#pizza-button').on('click', function () {
-  event.preventDefault()
-  pushItems(1)
-  let pizzaRow = `<div class="row list-item-unindented"><small id="pizza-label">Arugula Pie</small>
-  </div>`
-  let pizzaPriceRow = `<div class="row list-item-unindented"><small>$11.99</small></div>`
-  $(itemNameDiv).prepend(pizzaRow)
-  $(itemPriceDiv).prepend(pizzaPriceRow)
-
-  //replace totals
-  $('#subtotal-amnt').html('$' + calcSubTotal(items))
-  $('#tax-amnt').html('$' + calcTax())
-  $('#total-amnt').html('$' + calcTotal())
-})
-$('#ribs-button').on('click', function () {
-  event.preventDefault()
-  pushItems(2)
-  let ribsRow = `<div class="row list-item-unindented"><small id="ribs-label">Smoked Swine</small>
-  </div>`
-  let ribsPriceRow = `<div class="row list-item-unindented"><small>$14.99</small></div>`
-  $(itemNameDiv).prepend(ribsRow)
-  $(itemPriceDiv).prepend(ribsPriceRow)
-
-  //replace totals
-  $('#subtotal-amnt').html('$' + calcSubTotal(items))
-  $('#tax-amnt').html('$' + calcTax())
-  $('#total-amnt').html('$' + calcTotal())
-})
-$('#ice-cream-button').on('click', function () {
-  event.preventDefault()
-  pushItems(3)
-  let iceCreamRow = `<div class="row list-item-unindented"><small id="ice-cream-label">Ice Cream Biscut</small>
-  </div>`
-  let iceCreamPriceRow = `<div class="row list-item-unindented"><small>$7.99</small></div>`
-  $(itemNameDiv).prepend(iceCreamRow)
-  $(itemPriceDiv).prepend(iceCreamPriceRow)
-
-
-  //replace totals
-  $('#subtotal-amnt').html('$' + calcSubTotal(items))
-  $('#tax-amnt').html('$' + calcTax())
-  $('#total-amnt').html('$' + calcTotal())
-})
 
 $('#home').load("index.html")
 
